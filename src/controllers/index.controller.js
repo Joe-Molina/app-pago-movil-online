ce214f2f397df3635fcf46ba3b7960051aa1282a
Joe Molina <joedodaniljr123@gmail.com>
2023-09-15 14:48:49 -0400

    init

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,11 @@
+import bodyParser from "body-parser";
+import axios from "axios";
+import moment from "moment";
+import session from "express-session";
+import myConnection from "express-myconnection";
+import pool from "mysql2";
+
+export const getIndex = async (req, res) => {
+  try {
+  } catch (err) {}
+};
---
+++ src/controllers/login.controller.js
@@ -0,0 +1,49 @@
+import bodyParser from "body-parser";
+import myConnection from "express-myconnection";
+import session from "express-session";
+
+export const getLogin = async (req, res) => {
+  if (req.session.loggedin != true) {
+    res.render("login", { error: false });
+  } else {
+    res.redirect("/");
+  }
+};
+
+export const postLogin = async (req, res) => {
+  const data = req.body;
+
+  req.getConnection((err, conn) => {
+    conn.query(
+      "SELECT * FROM users WHERE email = ?",
+      [data.email],
+      (err, userdata) => {
+        if (userdata.length > 0) {
+          const password = data.password;
+          const hash = userdata[0].password;
+
+          if (password !== hash) {
+            res.render("login", { error: "incorrect password" });
+          } else {
+            req.session.loggedin = true;
+            req.session.user = userdata[0].id;
+            req.session.name = userdata[0].name;
+            req.session.email = userdata[0].email;
+            req.session.userType = userdata[0].tipo_usuario;
+
+            res.redirect("/");
+          }
+        } else {
+          res.render("login", { error: "el usuario no existe" });
+        }
+      }
+    );
+  });
+};
+
+export const getLogOut = async (req, res) => {
+  if (req.session.loggedin == true) {
+    req.session.destroy();
+  }
+  res.redirect("/login");
+};
